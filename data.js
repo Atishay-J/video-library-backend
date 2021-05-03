be1f0f53e0f90c256b7902c56e443b8d1609c553
Atishay Jain <hritikjain707@gmail.com>
2021-05-04 00:52:45 +0530

    init: Test Data

---
+++ data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    creatorId: 10,
+    isUserSubscribed: false,
+    creatorName: "Alex rome",
+    creatorVideos: [
+      {
+        videoId: 123,
+        videoTitle: "Init Name",
+        videoDuration: "5:10",
+        videoURL: "htpps://xyz",
+      },
+      {
+        videoId: 1234,
+        videoTitle: "Init Name 2",
+        videoDuration: "15:10",
+        videoURL: "htpps://xyz",
+      },
+    ],
+  },
+  {
+    creatorId: 11,
+    isUserSubscribed: false,
+    creatorName: "Alex rome cousin",
+    creatorVideos: [
+      {
+        videoId: 223,
+        videoTitle: "Init Name 11",
+        videoDuration: "5:10",
+        videoURL: "htpps://xyz",
+      },
+      {
+        videoId: 2234,
+        videoTitle: "Init Name 21",
+        videoDuration: "1:10",
+        videoURL: "htpps://xyz",
+      },
+    ],
+  },
+];
