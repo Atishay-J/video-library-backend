8bf09eae6889ccd40e6d3d9c4e80c48f7802ec28
Atishay Jain <hritikjain707@gmail.com>
2021-05-04 11:48:29 +0530

    Chore: Data Curated

--- data.js
+++ data.js
@@ -1,40 +1,131 @@
 const data = [
   {
-    creatorId: 10,
     isUserSubscribed: false,
-    creatorName: "Alex rome",
+    creatorName: "Pianote",
+    creatorAvatar: "https://bit.ly/3b2yT2N",
     creatorVideos: [
       {
-        videoId: 123,
-        videoTitle: "Init Name",
-        videoDuration: "5:10",
-        videoURL: "htpps://xyz",
+        videoId: "hMZXQ87OeuU",
+        videoTitle: "Beautiful Arpeggio Patterns for Beginners (Piano Lesson)",
+        videoDuration: "6:00",
+        videoTag: "piano",
       },
       {
-        videoId: 1234,
-        videoTitle: "Init Name 2",
-        videoDuration: "15:10",
-        videoURL: "htpps://xyz",
+        videoId: "8n7LsgFfFC4",
+        videoTitle: "The 1-5-6-4 Chord Progression (Piano Lesson)",
+        videoDuration: "3:44",
+        videoTag: "piano",
+      },
+      {
+        videoId: "ulq7HkupBSw",
+        videoTitle: "4 Things A Beginner Piano Player Should Practice",
+        videoDuration: "8:32",
+        videoTag: "piano",
+      },
+      {
+        videoId: "vzujwexshe4",
+        videoTitle: "Creating Beautiful Minor Chord Progressions",
+        videoDuration: "5:08",
+        videoTag: "piano",
+      },
+    ],
+  },
+
+  {
+    isUserSubscribed: false,
+    creatorName: "YourGuitarSage",
+    creatorAvatar: "https://bit.ly/2QUvyfh",
+    creatorVideos: [
+      {
+        videoId: "PPEsXb4XpyY",
+        videoTitle: "How to Tune Your Guitar Without a Tuner For Beginners",
+        videoDuration: "4:09",
+        videoTag: "guitar",
+      },
+      {
+        videoId: "xmX795qdAuw",
+        videoTitle: "Top 5 Things Every Beginner Guitar Player Should Know",
+        videoDuration: "12:39",
+        videoTag: "guitar",
+      },
+      {
+        videoId: "CmYzZu5h_Mo",
+        videoTitle:
+          "These 2 Techniques Cure All Your Chord Transitioning Troubles",
+        videoDuration: "13:31",
+        videoTag: "guitar",
+      },
+      {
+        videoId: "p8RyqWY0Izw",
+        videoTitle:
+          "CAGED System - Advanced Exercises & Applications - Free Bonus Course - Full Lesson Preview",
+        videoDuration: "11:07",
+        videoTag: "guitar",
       },
     ],
   },
   {
-    creatorId: 11,
     isUserSubscribed: false,
-    creatorName: "Alex rome cousin",
+    creatorName: "Rick Beato",
+    creatorAvatar: "https://bit.ly/3eg901c",
     creatorVideos: [
       {
-        videoId: 223,
-        videoTitle: "Init Name 11",
-        videoDuration: "5:10",
-        videoURL: "htpps://xyz",
+        videoId: "re4Rx0j2eMI",
+        videoTitle: "Basics of Music Theory: Part I",
+        videoDuration: "6:01",
+        videoTag: "music theory",
       },
       {
-        videoId: 2234,
-        videoTitle: "Init Name 21",
-        videoDuration: "1:10",
-        videoURL: "htpps://xyz",
+        videoId: "8iknqGaI7kQ",
+        videoTitle: "MUSIC THEORY: Where To Start? | Beginners Guide",
+        videoDuration: "31:10",
+        videoTag: "music theory",
+      },
+      {
+        videoId: "bFtTvqASQ9k",
+        videoTitle: "My Method to Learning ANY Song By Ear",
+        videoDuration: "10:12",
+        videoTag: "music theory",
+      },
+      {
+        videoId: "HIiKbSBv7RU",
+        videoTitle: "Learn Any Song By Ear (Even Complex Ones)",
+        videoDuration: "31:50",
+        videoTag: "music theory",
+      },
+    ],
+  },
+  {
+    isUserSubscribed: false,
+    creatorName: "ANDREW HUANG",
+    creatorAvatar: "https://bit.ly/3eejf6c",
+    creatorVideos: [
+      {
+        videoId: "57avc_R2MLE",
+        videoTitle: "Music with the lowest sound humans can hear",
+        videoDuration: "9:09",
+        videoTag: "music production",
+      },
+      {
+        videoId: "mlgkSrFq6hk",
+        videoTitle: "SAMPLING THE SPACESHIP",
+        videoDuration: "6:32",
+        videoTag: "music production",
+      },
+      {
+        videoId: "O-aQA1HKctQ",
+        videoTitle: "Making music without hearing it",
+        videoDuration: "10:25",
+        videoTag: "music production",
+      },
+      {
+        videoId: "aOwaNtVMQsk",
+        videoTitle: "Making a song in 59 minutes",
+        videoDuration: "12:16",
+        videoTag: "music production",
       },
     ],
   },
 ];
+
+// https://img.youtube.com/vi/TrdpK8dMsHY/0.jpg
